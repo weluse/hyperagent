a34d89889b82e022b341c3505340b3c93bd00836
Pascal Hartig <phartig@rdrei.net>
2013-05-08 16:29:16 +0200

    Barebones example

--- sample/app.js
+++ sample/app.js
@@ -4,18 +4,10 @@ function HALCtrl($scope, $location) {
   'use strict';
 
   $scope.location = $location;
-  $scope.users = [{
-    "href": "/users/graste",
-    "title": "gra ste"
-  },
-  {
-    "href": "/users/mamund",
-    "title": "Mike Amundsen"
-  },
-  {
-    "href": "/users/amir",
-    "title": "Amir Mohtasebi"
-  }];
+  $scope.posts = [];
+  $scope.user = null;
+  $scope.users = [];
+
   $scope.$watch('location.path()', function (path) {
     console.log('New Pathhhh: ', path);
   });
