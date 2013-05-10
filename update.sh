618c1f7f2ab69536148eac01b31c45f7adf1f988
Pascal Hartig <phartig@rdrei.net>
2013-05-11 01:49:47 +0200

    Use relative paths

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ for file in README CONTRIBUTING INSTALL; do
   git show master:${file}.md > _includes/${file}.md
 done
 bundle
-bundle exec compass compile
+bundle exec compass compile --relative-assets
 bundle exec jekyll build
