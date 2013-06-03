3c41b277e4c5d4a74d7e495cbab8dc244929e901
Pascal Hartig <phartig@rdrei.net>
2013-06-03 23:43:37 +0200

    Added version number

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+LATEST_TAG=$(git describe --always --tags master)
+
 mkdir -p _includes/
 for file in README CONTRIBUTING INSTALL; do
   git show master:${file}.md > _includes/${file}.md
@@ -11,6 +13,8 @@ for dir in dist sample; do
   rm -r $dir
   git checkout master -- $dir
 done
+
+sed -ei "s:<var id=\"hyperagent-version\">.*</var>var>:<var id=\"hyperagent-version\">$LATEST_TAG</var>var>:" _layouts/page.html
 
 bundle
 bundle exec compass compile --relative-assets --force
