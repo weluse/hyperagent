1917696e1bb5cb3b53a2d33a1111c14d86e4eeea
Pascal Hartig <phartig@rdrei.net>
2013-07-13 15:47:40 +0200

    Upgraded to karma 0.9

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -1,68 +1,74 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function (config) {
+  'use strict';
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'components/chai/chai.js',
-  'components/uri.js/src/URI.js',
-  'components/uri.js/src/URITemplate.js',
-  'components/q/q.js',
-  'dist/hyperagent.js',
-  'test/setup.js',
-  'test/lib/utils.js',
-  'test/fixtures/**/*.js',
-  'test/spec/**/*.js'
-];
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
+    frameworks: ['mocha'],
 
-preprocessors = {
-  'dist/hyperagent.js': 'coverage'
+    // list of files / patterns to load in the browser
+    files: [
+      'components/chai/chai.js',
+      'components/uri.js/src/URI.js',
+      'components/uri.js/src/URITemplate.js',
+      'components/q/q.js',
+      'dist/hyperagent.js',
+      'test/setup.js',
+      'test/lib/utils.js',
+      'test/fixtures/**/*.js',
+      'test/spec/**/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [],
+
+    preprocessors: {
+      'dist/hyperagent.js': 'coverage'
+    },
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['coverage', 'progress'],
+
+    // web server port
+    port: 8080,
+
+    // cli runner port
+    runnerPort: 9100,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['coverage', 'progress'];
-
-coverageReporter = {
-  type: 'lcov',
-  dir: 'coverage/'
-}
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
--- karma.conf.js
+++ karma.conf.js
@@ -1,59 +1,65 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function (config) {
+  'use strict';
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'components/chai/chai.js',
-  'components/uri.js/src/URI.js',
-  'components/uri.js/src/URITemplate.js',
-  'components/q/q.js',
-  'dist/hyperagent.js',
-  'test/setup.js',
-  'test/lib/utils.js',
-  'test/fixtures/**/*.js',
-  'test/spec/**/*.js'
-];
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
+    frameworks: ['mocha'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // list of files / patterns to load in the browser
+    files: [
+      'components/chai/chai.js',
+      'components/uri.js/src/URI.js',
+      'components/uri.js/src/URITemplate.js',
+      'components/q/q.js',
+      'dist/hyperagent.js',
+      'test/setup.js',
+      'test/lib/utils.js',
+      'test/fixtures/**/*.js',
+      'test/spec/**/*.js'
+    ],
 
-// web server port
-port = 8080;
+    // list of files to exclude
+    exclude: [],
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // cli runner port
+    runnerPort: 9100,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS', 'Chrome', 'Firefox'];
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
