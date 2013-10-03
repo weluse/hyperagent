c482bae158c66f1a478384df1570a3dcd7efd258
Nico Mihalich <nico@adstage.io>
2013-10-03 10:47:52 -0700

    undo dist changes

--- dist/amd/hyperagent/loader.js
+++ dist/amd/hyperagent/loader.js
@@ -6,13 +6,11 @@ define("/hyperagent/loader",
 
     function loadAjax(options) {
       var deferred = config.defer();
-      if (options.headers) {
-        config._.extend(options.headers,{
+      config.ajax(config._.extend({
+        headers: {
           'Accept': 'application/hal+json, application/json, */*; q=0.01',
           'X-Requested-With': 'XMLHttpRequest'
-        })
-      }
-      config.ajax(config._.extend({
+        },
         success: deferred.resolve,
         error: deferred.reject,
         dataType: 'html'  // We don't want auto-converting
--- dist/commonjs/hyperagent/loader.js
+++ dist/commonjs/hyperagent/loader.js
@@ -3,13 +3,11 @@ var config = require("hyperagent/config").config;
 
 function loadAjax(options) {
   var deferred = config.defer();
-  if (options.headers) {
-    config._.extend(options.headers,{
+  config.ajax(config._.extend({
+    headers: {
       'Accept': 'application/hal+json, application/json, */*; q=0.01',
       'X-Requested-With': 'XMLHttpRequest'
-    })
-  }
-  config.ajax(config._.extend({
+    },
     success: deferred.resolve,
     error: deferred.reject,
     dataType: 'html'  // We don't want auto-converting
