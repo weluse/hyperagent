ddf28116f498fde984d5896f0424c266d6cea241
Nico Mihalich <nico@adstage.io>
2013-10-03 10:21:51 -0700

    lint fixes

--- dist/hyperagent.js
+++ dist/hyperagent.js
@@ -153,11 +153,13 @@ define("hyperagent/loader",
 
     function loadAjax(options) {
       var deferred = config.defer();
-      config.ajax(config._.extend({
-        headers: {
+      if (options.headers) {
+        config._.extend(options.headers,{
           'Accept': 'application/hal+json, application/json, */*; q=0.01',
           'X-Requested-With': 'XMLHttpRequest'
-        },
+        })
+      }
+      config.ajax(config._.extend({
         success: deferred.resolve,
         error: deferred.reject,
         dataType: 'html'  // We don't want auto-converting
